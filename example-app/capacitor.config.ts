d8a03d8955fdaa6519c118793f76aef835bf5242
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:33 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,22 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.contentsquare.example",
-  "appName": "Contentsquare Example",
-  "webDir": "dist",
-  "plugins": {
-    "SplashScreen": {
-      "launchAutoHide": false
+  appId: 'app.capgo.contentsquare.example',
+  appName: 'Contentsquare Example',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
     },
-    "CapacitorUpdater": {
-      "appId": "app.capgo.contentsquare.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapacitorUpdater: {
+      appId: 'app.capgo.contentsquare.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
